72ba14160a9e0b91071ae1b0106e6a6880656612
hifza <hifzanazir456@gmail.com>
2023-10-21 21:24:18 +0500

    conert while loop into for loop

--- src/exercises.c
+++ src/exercises.c
@@ -36,9 +36,14 @@ int main(int argc, char **argv) {
   }
   printf("\n");
   //2. Rewrite the given while loop as an equivalent for loop
-  int k = 3;
-  while(k > -4) {
-    printf("%d ", k);
+  // int k = 3;
+  // while(k > -4) {
+  //   printf("%d ", k);
+  //   k--;
+  // }
+  for(int k=3;k>-4;k--)
+  {
+    printf("%d",k);
     k--;
   }
   printf("\n");
