39c7b592f365d52bf053f3a4f9671993671994f5
hifza <hifzanazir456@gmail.com>
2023-10-22 17:05:36 +0500

    result 7 : summation of even number 1 thru n

--- src/exercises.c
+++ src/exercises.c
@@ -112,7 +112,19 @@ int main(int argc, char **argv) {
 
   //7. Compute the summation of even numbers 1 thru n and print
   //   the result; example (n=5): 2 + 4 = 6
-
+  int result_7=0;
+  for(int i=1;i<=n;i++)
+  {
+    if(i%2!=0)
+    {
+      continue;
+    }
+    else
+    {
+      result_7+=i;
+    }
+  }
+  printf("\n");
   //8. Write a solution to the following variation of the FizzBuzz
   //   problem.  Print out integers 1 thru n, one to a line, except
   //   that if the integer is divisible by 7 print "Foo" instead.
